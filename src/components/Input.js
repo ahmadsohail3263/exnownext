701618a3ae92c3263777e88dd1bb69a0a0a31a92
Ahmad Sohail Usman <ahmadsohailusman@gmail.com>
2024-08-23 20:46:51 +0500

    resolve error

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,15 +3,17 @@ import React, { forwardRef } from 'react';
 // Forward the ref to the input element
 const Input = forwardRef(({ title, ...props }, ref) => (
   <div className="relative z-0 w-full mb-5 group">
-    <label className={`block text-sm font-medium text-gray-700`}>
+    <label className="block text-sm font-medium text-gray-700">
       {title}
     </label>
     <input
       {...props}
       ref={ref} // Forward the ref
-      className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-[#2C7B63] focus:border-[#2C7B63] sm:text-sm`}
+      className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-[#2C7B63] focus:border-[#2C7B63] sm:text-sm"
     />
   </div>
 ));
+
+Input.displayName = 'Input';
 
 export default Input;
